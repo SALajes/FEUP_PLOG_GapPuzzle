c517d52edba3c5af55e610e45641f831a5082530
SALajes <sofialajes98@gmail.com>
2019-12-27 14:51:07 +0000

    board display completed

--- src/display.pl
+++ src/display.pl
@@ -1,34 +1,60 @@
 :- use_module(library(ansi_term)).
 :- use_module(library(lists)).
-:- include('internalstructure.pl').
-:- include('utilities.pl').
 
-%When the board is empty, prints the last line delimiter 
-%(the consecutive dashes "-----")
-display_board([], Row) :-
-    print_dashed_line(Row),
-    nl.
+display(Board, Restrictions):-
+    length(Board, N),
+    print_board(Board, N, Restrictions, 1).
 
-%Depending on the value or Row, prints the respective dashed line and 
-%the line with the board contents
-display_board([H|T], Row) :-
-    print_dashed_line(Row),
-    nl,
-    Col is 1,
-    print_row(H, Row, Col),
-    nl,
-    Row2 is Row+1,
-    display_board(T, Row2).
+print_board([], Dimensions, [_|T], _):-
+    nth1(1, T, ColumnRestrictions),
+    printRestrictionedColumns(1, Dimensions, ColumnRestrictions),
+    !.
 
-%When there are no more columns of the same line to print, 
-%prints the last vertical bar
-print_row([], _, _) :-
-    write('|').
+print_board([H|T], Dimensions, Restrictions, Row) :-
+  writeRow(H), 
+  isRestrictionedRow(Restrictions, Row),
+  nl,
+  NextRow is Row + 1,
+  print_board(T, Dimensions, Restrictions, NextRow).
 
-%Depending on the Col of the same Row (Row is constant),  
-%prints the respective cell
-print_row([H|T], Row, Col) :-
-    print_pipe(Row, Col),
-    print_cell(H, Row, Col),
-    Col2 is Col+1,
-    print_row(T, Row, Col2).
+%-----------print restrictions-------------
+%Row
+isRestrictionedRow([H|_], Row):-
+    search(H, Row).
+
+%column
+printRestrictionedColumns(Column, Column, _):- !.
+
+printRestrictionedColumns(Column, Dimensions, Restrictions):-
+    search(Restrictions, Column),
+    NextColumn is Column + 1,
+    printRestrictionedColumns(NextColumn, Dimensions, Restrictions),
+    !.
+    
+%search restrictions for a specific row or columns
+search([], _):- write('  ').
+
+search([[N|T]|_], N):-
+    write(' '),
+    nth1(1,T,R),
+    write(R),
+    !.
+
+search([_|T], N):-
+    search(T, N).
+    
+
+%write cells
+writeRow([]).
+writeRow([H|T]):-
+    define_color(H, Color),
+    print_cell(Color),
+    writeRow(T).
+
+define_color(1, black):- !.
+define_color(0, white).
+
+print_cell(Color):-
+    write(' '), 
+    ansi_format([fg(Color)], '~c', [9606]).
+
