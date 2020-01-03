8d7a66888b74a4071b38c3d711adc6ddfc0c83f8
SALajes <sofialajes98@gmail.com>
2020-01-03 00:26:38 +0000

    added gap, added column restrictions, generator and interface

--- src/solver.pl
+++ src/solver.pl
@@ -1,51 +1,75 @@
-:- use_module(library(lists)).
 :- use_module(library(clpfd)).
 
 :- include('display.pl').
+
+%delete this
 :- include('internal_structure.pl').
+
 
 %--------------
 restrict([], _, _):-!.
-restrict(Board, Size, Size) :-
+restrict([Row|_], Size, Size, [RowRestrictions|_]) :-
   Zeros is Size - 2,
   Ones is 2,
-  nth1(Size, Board, Row),
   global_cardinality(Row, [0-Zeros, 1-Ones]),
   element(I1, Row, 1), element(I2, Row, 1),
-  I1 #< I2 - 1,
-  !.
+  gap_restriction(Size, I1, I2, RowRestrictions).
+  
 
-restrict(Board, Index, Size) :-
+restrict([Row, NextRow|T], Index, Size, [RowRestrictions|_]) :-
   Zeros is Size - 2,
   Ones is 2,
-  nth1(Index, Board, Row),
+  NextIndex is Index + 1,
   global_cardinality(Row, [0-Zeros, 1-Ones]),
   element(I1, Row, 1), element(I2, Row, 1),
-  I1 #< I2 - 1,
-  NextIndex is Index + 1,
-  nth1(NextIndex, Board, NextRow),
+  gap_restriction(Index, I1, I2, RowRestrictions),
   element(I3, NextRow, 1), element(I4, NextRow, 1),
-  I3 #< I4 - 1,
+  abs(I3 - I4) #> 1,
   abs(I1 - I3) #> 2,
   abs(I1 - I4) #> 2,
   abs(I2 - I3) #> 2,
   abs(I2 - I4) #> 2,
-  restrict(Board, NextIndex, Size).
+  restrict([NextRow|T], NextIndex, Size, [RowRestrictions|_]).
 
-restrict_transpose([], _):-!.
-restrict_transpose([H|T], Size) :-
+%restrict columns
+restrict_columns([], _,_,_).
+restrict_columns([Column|T], Index, Size, [_|ColumnRestrictions]) :-
   Zeros is Size - 2,
   Ones is 2,
-  global_cardinality(H, [0-Zeros, 1-Ones]),
-  restrict_transpose(T, Size).
+  global_cardinality(Column, [0-Zeros, 1-Ones]),
+  element(I1, Column, 1), element(I2, Column, 1),
+  gap_restriction(Index, I1, I2, ColumnRestrictions),
+  NextIndex is Index + 1,
+  restrict_columns(T, NextIndex, Size, [_|ColumnRestrictions]).
 
-puzzle(Variables, Dimension) :-
-  Restrictions = [[[4,5]],[[1,2]]],
+%set row or column gap
+gap_restriction(Index, I1, I2, Restrictions):-
+  get_gap(Index, Restrictions, Gap),
+  abs(I1 - I2) #= Gap + 1,
+  !.
+
+gap_restriction(_, I1, I2, _):-
+  abs(I1 - I2) #> 1.
+
+get_gap(_, [], _):- false.
+get_gap(Index, [H|_], Gap):-
+  nth1(1,H,Index),
+  nth1(2,H,Gap),
+  !.
+
+get_gap(Index,[_|T], Gap):-
+  get_gap(Index, T, Gap).
+
+
+%-----SOLVER-----
+%solver(Variables, Dimension, Board, Restrictions) :-
+solver(Variables, Dimension) :-
+  Restrictions = [[[9,4]],[[3,4]]],
   init_board(Board, Dimension),
   append(Board, Variables),
   domain(Variables, 0, 1),
-  restrict(Board, 1, Dimension),
+  restrict(Board, 1, Dimension, Restrictions),
   transpose(Board, NewBoard),
-  restrict_transpose(NewBoard, Dimension),
+  restrict_columns(NewBoard, 1, Dimension, Restrictions),
   labeling([], Variables),
   print_board(Board, Dimension, Restrictions, 1).
