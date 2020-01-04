0ab30ae2af8492839a29cacf95a87309272cadd4
Carlos Duarte <cnova1998@gmail.com>
2020-01-04 13:39:02 +0000

    Progress on test.pl

--- test.pl
+++ test.pl
@@ -11,17 +11,6 @@ print_board([H|T]) :-
     write(H), nl, 
     print_board(T).
 
-constrain([I1, I2]) :-
-    abs(I1 - I2) #> 1.
-
-constrain([I1, I2, I3, I4|T]) :-
-    abs(I1 - I2) #> 1,
-    abs(I1 - I3) #> 1,
-    abs(I1 - I4) #> 1,
-    abs(I2 - I3) #> 1,
-    abs(I2 - I4) #> 1,
-    constrain([I3, I4|T]).
-
 print_line([], _).
 print_line([H1, H2|T], Counter) :-
     Counter2 is Counter + 1,
@@ -29,30 +18,32 @@ print_line([H1, H2|T], Counter) :-
     NextCounter is Counter2 + 1,
     print_line(T, NextCounter).
 
-% get_column_values([], _, _, AuxList, AuxList).
-% get_column_values([Value|T], Value, Counter, [Counter|AuxList], List).
-% get_column_values([_|T], Value, Counter, AuxList, List) :-
-%     NewCounter is Counter + 1,
-%     get_column_values(T, Value, NewCounter, AuxList, List).
+constrain([V1, V2]) :-
+    abs(V1 - V2) #> 1.
 
-indices(List, E, Is) :-
-    findall(N, nth1(N, List, E), Is).
+constrain([V1, V2, V3, V4|T]) :-
+    abs(V1 - V2) #> 1,
+    abs(V1 - V3) #> 1,
+    abs(V1 - V4) #> 1,
+    abs(V2 - V3) #> 1,
+    abs(V2 - V4) #> 1,
+    constrain([V3, V4|T]).
 
 test(Variables, Size) :-
     NumVariables is Size*2,
     length(Variables, NumVariables),
     domain(Variables, 1, 9),
     global_cardinality(Variables, [1-2, 2-2, 3-2, 4-2, 5-2, 6-2, 7-2, 8-2, 9-2]),
+    
+    element(11, Variables, V11), element(12, Variables, V12),
+    abs(V11 - V12) #= 4,
 
-    element(15, Variables, I15), element(16, Variables, I16),
-    abs(I15 - I16) #= 5,
-
-    element(IX, Variables, 3),
-    element(IY, Variables, 3),
-    IX #\= IY,
-    abs(IX - IY) #= 6,
+    element(IX, Variables, 1),
+    element(IY, Variables, 1),
+    IY #>= IX,
+    div((IY - IX), 2) #= 5,
 
     constrain(Variables),
-    
+
     labeling([], Variables),
     print_line(Variables, 1).
